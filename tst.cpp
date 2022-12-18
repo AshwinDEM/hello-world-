e629eed1392edbd51c362d0f4bb75a3a32369df3
Ashwin1314 <40566417+Ashwin1314@users.noreply.github.com>
2022-12-18 09:19:15 +0530

    Add files via upload

---
+++ tst.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int sum=0;
+    
+    int arr[] ={};
+    for (int i =0;i<n;i++){
+        cout << "Enter a number:";
+        cin >> arr[i];
+        sum=sum+arr[i];
+    }
+    cout << sum;
+}
